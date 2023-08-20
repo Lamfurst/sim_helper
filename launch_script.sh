a99fd041b8a7c7c664263d5ff251a7a9ef407bdb
Lamfurst <lamfurst@outlook.com>
2023-08-19 21:41:03 -0400

    Updated complete launch_script.sh

--- launch_script.sh
+++ launch_script.sh
@@ -1,11 +1,35 @@
+# #!/bin/bash
+# cd gnss_localizer
+# ./launch_script.sh
+# sleep 5
+
+# cd ../traffic_light_bridge
+# ./launch_script.sh 
+
+# cd ../auto_reroute_py
+# ./launch_script.sh
+
 #!/bin/bash
+
+use_gnss=false
+
+for arg in "$@"; do
+  case $arg in
+    --gnss=true)
+      use_gnss=true
+      ;;
+  esac
+done
+
 cd gnss_localizer
-./launch_script.sh
+if $use_gnss; then
+  ./launch_script.sh
+fi
 sleep 5
 
 cd ../traffic_light_bridge
 ./launch_script.sh 
 
 cd ../auto_reroute_py
-./launch_script.sh true
+./launch_script.sh
 
