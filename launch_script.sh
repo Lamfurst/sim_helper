d2a69e61a4640971880a98b3813d6d2aeb366716
Lamfurst <lamfurst@outlook.com>
2023-08-19 21:45:06 -0400

    Update launch

--- launch_script.sh
+++ launch_script.sh
@@ -11,25 +11,43 @@
 
 #!/bin/bash
 
+#!/bin/bash
+
 use_gnss=false
+use_traffic_light=false
+use_auto_reroute=false
 
 for arg in "$@"; do
   case $arg in
     --gnss=true)
       use_gnss=true
       ;;
+    --traffic-light=true)
+      use_traffic_light=true
+      ;;
+    --auto-reroute=true)
+      use_auto_reroute=true
+      ;;
   esac
 done
 
-cd gnss_localizer
 if $use_gnss; then
+  cd gnss_localizer
   ./launch_script.sh
+  sleep 5
+  cd ..
 fi
-sleep 5
 
-cd ../traffic_light_bridge
-./launch_script.sh 
+if $use_traffic_light; then
+  cd traffic_light_bridge
+  ./launch_script.sh
+  cd ..
+fi
 
-cd ../auto_reroute_py
-./launch_script.sh
+if $use_auto_reroute; then
+  cd auto_reroute_py
+  ./launch_script.sh
+  cd ..
+fi
+
 
