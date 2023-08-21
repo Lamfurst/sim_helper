69d8fa4cb479c0e9cb55e54ca0a209a93693fa35
Lamfurst <lamfurst@outlook.com>
2023-08-21 16:39:21 -0400

    Added map files

---
+++ autoware_map/Town01/Downsampling/voxel_grid.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+
+int main ()
+{
+  pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2 ());
+  pcl::PCLPointCloud2::Ptr cloud_filtered (new pcl::PCLPointCloud2 ());
+
+  // Fill in the cloud data
+  pcl::PCDReader reader;
+  // Replace the path below with the path where you saved your file
+  reader.read ("/home/artemis/Workspace/autowarefoundation/map_data/Town01/Town01.pcd", *cloud); // Remember to download the file first!
+  
+  float leaf_size;
+  std::cout << "Enter the leaf size: "; 
+  std::cin >> leaf_size;
+
+  std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height 
+       << " data points (" << pcl::getFieldsList (*cloud) << ")." << std::endl;
+
+  // Create the filtering object
+  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+  sor.setInputCloud (cloud);
+  // sor.setLeafSize (0.5f, 0.5f, 0.5f);
+  sor.setLeafSize (leaf_size, leaf_size, leaf_size);
+  sor.filter (*cloud_filtered);
+
+  std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height 
+       << " data points (" << pcl::getFieldsList (*cloud_filtered) << ")." << std::endl;
+
+  pcl::PCDWriter writer;
+  writer.write ("/home/artemis/Workspace/autowarefoundation/map_data/Town01/pointcloud_map.pcd", *cloud_filtered, 
+         Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
+
+  return (0);
+}
